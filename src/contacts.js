b4271f225961ed6e0f72321c5450736dfe3c7e2c
Kristina <kasyankn@gmail.com>
2022-05-18 16:07:06 +0300

    Задание по оптимизации. Исправление

--- src/contacts.js
+++ src/contacts.js
@@ -1,37 +1,20 @@
-var contacts = document.getElementsByClassName("contacts")[0];
+const contacts = document.getElementsByClassName("contacts")[0];
 const stickyHeader = document.getElementsByClassName("stickyHeader")[0];
-var k = 0
-var n = 200
-function addContacts(k,n) {
-  if (n > 50000)
-    n=50000
-  const fragment = document.createDocumentFragment();
-  for (let i = k; i < n; i++) {
-    const child = document.createElement("div");
-    child.textContent = i;
-    child.classList.add("contact");
-    fragment.appendChild(child);
-  }
-  contacts.appendChild(fragment);
+
+function addContacts() {
+   const fragment = document.createDocumentFragment();
+   for (let i = 0; i < 50000; i++) {
+      const child = document.createElement("div");
+      child.textContent = i;
+      child.classList.add("contact");
+      fragment.appendChild(child);
+   }
+   contacts.appendChild(fragment);
 }
 
-contacts.addEventListener('scroll', function() {
-  var items = Array.from(contacts.getElementsByClassName("contact"));
-  const itemOffsets = items.map((item) => item.offsetTop);
-  
-  const topItemIndex = itemOffsets.findIndex(
-    (offset) => contacts.scrollTop - offset <= -18
-  );
-  if (topItemIndex !== -1) {
-    stickyHeader.textContent = items[topItemIndex].textContent;
-  }
-   
-  if (topItemIndex >= n*4/5) {
-    k+=200
-    n+=200
-    addContacts(k,n)
-  }
-
+//scrollTop измеряет расстояние от верха элемента до начала видимого контента.
+contacts.addEventListener("scroll", (e) => {
+   stickyHeader.textContent = Math.floor(contacts.scrollTop / 18);
 });
 
-addContacts(k,n);
+addContacts();
